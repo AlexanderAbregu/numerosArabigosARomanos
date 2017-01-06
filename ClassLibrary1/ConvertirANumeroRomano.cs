32a0818fbf2a1375a420cae2792fdbfab9c93a9a
Alexander Abregu <alexanderarmua@hotmail.com>
2017-01-06 12:27:45 -0300

    First Commit

---
+++ ClassLibrary1/ConvertirANumeroRomano.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class ConvertirANumeroRomano
+    {
+        private enum SimboloRomano
+        {
+            I = 1,
+            V = 5,
+            X = 10,
+            L = 50,
+            C = 100,
+            D = 500, 
+            M = 1000
+        }
+        public string PasarANumueroRomano(int numero)
+        {
+            String retornar = "";
+            if (numero < 1 || numero > 3999)
+            {
+                throw new IndexOutOfRangeException("El numero tiene debe estar entre [1;3000]");
+            }
+            else
+            {
+                if (Enum.IsDefined(typeof(SimboloRomano), numero))
+                {
+                    retornar = ((SimboloRomano)numero).ToString();
+                }
+                else
+                {
+
+                    while (numero != 0)
+                    {
+                        if (numero - (int)SimboloRomano.M >= 0)
+                        {
+                            numero -= (int)SimboloRomano.M;
+                            retornar += ((SimboloRomano)(int)SimboloRomano.M).ToString();
+                        } 
+                        else if (numero - (int)SimboloRomano.D >= 0)
+                        {
+                            if (numero >= 900)
+                            {
+                                numero += 100;
+                                retornar += "C";
+                            }
+                            else
+                            {
+                                numero -= (int)SimboloRomano.D;
+                                retornar += ((SimboloRomano)(int)SimboloRomano.D).ToString();
+                            }
+                        }
+                        else if (numero - (int)SimboloRomano.C >= 0)
+                        {
+                            if (numero >= 400)
+                            {
+                                numero += 100;
+                                retornar += "C";
+                            }
+                            else
+                            {
+                                numero -= (int)SimboloRomano.C;
+                                retornar += ((SimboloRomano)(int)SimboloRomano.C).ToString();
+                            }
+                        }
+                        else if (numero - (int)SimboloRomano.L >= 0)
+                        {
+                            if (numero >= 90)
+                            {
+                                numero += 10;
+                                retornar += "X";
+                            }
+                            else
+                            {
+                                numero -= (int)SimboloRomano.L;
+                                retornar += ((SimboloRomano)(int)SimboloRomano.L).ToString();
+                            }
+                        }
+                        else if (numero - (int)SimboloRomano.X >= 0)
+                        {
+                            if (numero >= 40)
+                            {
+                                numero += 10;
+                                retornar += "X";
+                            }
+                            else
+                            {
+                                numero -= (int)SimboloRomano.X;
+                                retornar += ((SimboloRomano)(int)SimboloRomano.X).ToString();                 
+                            }
+                        }
+                        else if (numero - (int)SimboloRomano.V >= 0)
+                        {
+                            if (numero >= 9)
+                            {
+                                numero += 1;
+                                retornar += "I";
+                            }
+                            else
+                            {
+                                numero -= (int)SimboloRomano.V;
+                                retornar += ((SimboloRomano)(int)SimboloRomano.V).ToString();
+                            }
+                            
+                        }
+                        else if (numero - (int)SimboloRomano.I >= 0)
+                        {
+                            if (numero >= 4)
+                            {
+                                numero += 1;
+                                retornar += "I";
+                            }
+                            else
+                            {
+                                numero -= (int)SimboloRomano.I;
+                                retornar += ((SimboloRomano)(int)SimboloRomano.I).ToString();
+                            }
+                        }
+                    }
+                    
+
+                              
+                }
+                
+            }
+
+            return retornar;
+        }
+    }
+}
---
+++ Unit Test Project 1/UnitTest1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace NumberSystemConverter_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumeroMasGrandeQue3Mil()
+        {
+            var converter = new ConvertirANumeroRomano();
+            converter.PasarANumueroRomano(4000);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumeroMasChicoQue3Mil()
+        {
+            var converter = new ConvertirANumeroRomano();
+            converter.PasarANumueroRomano(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumeroNoPuedeSerCero()
+        {
+            var converter = new ConvertirANumeroRomano();
+            converter.PasarANumueroRomano(0);
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_01_Uno()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1);
+
+            Assert.AreEqual(resultado, "I");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_02_Cinco()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(5);
+
+            Assert.AreEqual(resultado, "V");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_03_Diez()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(10);
+
+            Assert.AreEqual(resultado, "X");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_04_Cincuenta()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(50);
+
+            Assert.AreEqual(resultado, "L");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_05_Cien()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(100);
+
+            Assert.AreEqual(resultado, "C");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_06_Quinientos()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(500);
+
+            Assert.AreEqual(resultado, "D");
+        }
+        
+        [TestMethod]
+        public void EsperabaQueSea_07_Mil()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1000);
+
+            Assert.AreEqual(resultado, "M");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_08_DosMil()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(2000);
+
+            Assert.AreEqual(resultado, "MM");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_09_TresMil()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(3000);
+
+            Assert.AreEqual(resultado, "MMM");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_10_TresCientoCincuenta()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(350);
+
+            Assert.AreEqual(resultado, "CCCL");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_11_SeteCientosCincuenta()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(750);
+
+            Assert.AreEqual(resultado, "DCCL");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_12_DosMilSetecientosCincuenta()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(2750);
+
+            Assert.AreEqual(resultado, "MMDCCL");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_13_Nueve()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(9);
+
+            Assert.AreEqual(resultado, "IX");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_14_TreintaYOcho()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(38);
+
+            Assert.AreEqual(resultado, "XXXVIII");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_15_MilQuinientosCincuenta()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1550);
+
+            Assert.AreEqual(resultado, "MDL");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_16_MilQuinientosCincuentaYUno()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1551);
+
+            Assert.AreEqual(resultado, "MDLI");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_17_MilQuinientosCincuentaYDos()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1552);
+
+            Assert.AreEqual(resultado, "MDLII");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_18_MilQuinientosCincuentaYTres()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1553);
+
+            Assert.AreEqual(resultado, "MDLIII");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_19_MilQuinientosCincuentaYCuatro()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1554);
+
+            Assert.AreEqual(resultado, "MDLIV");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_20_MilQuinientosCincuentaYCinco()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1555);
+
+            Assert.AreEqual(resultado, "MDLV");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_21_MilQuinientosCincuentaYSeis()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1556);
+
+            Assert.AreEqual(resultado, "MDLVI");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_22_MilQuinientosCincuentaYSiete()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1557);
+
+            Assert.AreEqual(resultado, "MDLVII");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_23_MilQuinientosCincuentaYOcho()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1558);
+
+            Assert.AreEqual(resultado, "MDLVIII");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_24_MilQuinientosCincuentaYNueve()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1559);
+
+            Assert.AreEqual(resultado, "MDLIX");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_25_MilQuinientosSesenta()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1560);
+
+            Assert.AreEqual(resultado, "MDLX");
+        }
+        
+        [TestMethod]
+        public void EsperabaQueSea_26_Cuatrocientos()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(400);
+
+            Assert.AreEqual(resultado, "CD");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_27_Novecientos()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(900);
+
+            Assert.AreEqual(resultado, "CM");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_28_CuarentaYNueve()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(49);
+
+            Assert.AreEqual(resultado, "XLIX");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_29_DieciNueve()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(19);
+
+            Assert.AreEqual(resultado, "XIX");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_30_TreintaYNueve()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(39);
+
+            Assert.AreEqual(resultado, "XXXIX");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_31_NoventaYCuatro()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(94);
+
+            Assert.AreEqual(resultado, "XCIV");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_32_MilCuarenta()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1040);
+
+            Assert.AreEqual(resultado, "MXL");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_33_MilCuatrocientos()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1400);
+
+            Assert.AreEqual(resultado, "MCD");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_34_MilNoventa()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1090);
+
+            Assert.AreEqual(resultado, "MXC");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_35_MilNovecientos()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1900);
+
+            Assert.AreEqual(resultado, "MCM");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_36_MilCuatrocientosNoventa()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1490);
+
+            Assert.AreEqual(resultado, "MCDXC");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_37_MilCuatrocientosNoventaYCuatro()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(1494);
+
+            Assert.AreEqual(resultado, "MCDXCIV");
+        }
+
+        [TestMethod]
+        public void EsperabaQueSea_38_TresMilNovecientosNoventaYNueve()
+        {
+            var converter = new ConvertirANumeroRomano();
+            var resultado = converter.PasarANumueroRomano(3999);
+
+            Assert.AreEqual(resultado, "MMMCMXCIX");
+        }
+    }
+}
